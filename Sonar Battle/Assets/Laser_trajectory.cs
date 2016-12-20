223ec08a9309f09503ca771fa02648f20ce546bf
ProjectSONAR <ProjectSONAR@email.com>
2016-12-20 14:30:52 -0500

    Hmm...I think I want to delete these files...

---
+++ Sonar Battle/Assets/Laser_trajectory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser_trajectory : MonoBehaviour {
+
+
+    public float movespeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.Translate(transform.up * movespeed);
+		
+	}
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "DefaultWall(Clone)")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
